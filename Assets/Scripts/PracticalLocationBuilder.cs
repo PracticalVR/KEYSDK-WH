428688bdb9034f375e98009bca7c4d0ddd14202f
softwarengineer2 <softwarengineer2@users.noreply.github.com>
2018-07-24 00:42:09 +0300

    Initialization

---
+++ Assets/Scripts/PracticalLocationBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PracticalManaged.Practical.SpatialSpawning;
+namespace Practical.Internal
+{
+    [Serializable]
+    public class PracticalLocationBuilder : PracticalLocator
+    {
+        private void Start()
+        {
+            PracticalLocator.Instance.DefinedGroups = PracticalLocationBuilder.Instance.DefinedGroups;
+        }
+    }
+}
